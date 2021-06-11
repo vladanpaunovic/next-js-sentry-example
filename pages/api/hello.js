0c2958b128ccc9bee5d25487555d5ed094172eec
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-06-11 08:50:31 +0200

    chore: init nextjs and sentry

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from "@sentry/nextjs";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = async (req, res) => {
+  throw new Error("New error from Vladan");
+  res.status(200).json({ name: "John Doe" });
+};
+
+export default withSentry(handler);
